73dcf09ecda4e1ebebc2be5baad2b27b8615a7af
Victor-Olufade <olufadevictor97@gmail>
2023-01-06 17:00:15 +0100

    create schemas for sanity queries

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+import user from './user'
+import comment from './comment'
+import pin from './pin'
+import save from './save'
+import postedBy from './postedBy'
+export const schemaTypes = [
+    user,
+    pin,
+    comment,
+    save,
+    postedBy
+]
